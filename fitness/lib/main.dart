8a7f17f2054bcc2043abdd31794d0431fe5f2a5f
hilawnabangus <jaymaryugo90@gmail.com>
2026-09-10 15:24:10 +0800

    remove debug banner

--- fitness/lib/main.dart
+++ fitness/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false, //remove the red debug banner
       home: HomePage()
     );
   }
