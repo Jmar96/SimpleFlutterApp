d61cfb5efd80d5590627b272d1eb04ed6328f155
hilawnabangus <jaymaryugo90@gmail.com>
2026-09-17 14:44:16 +0800

    working crud now

--- fitness/lib/pages/sample_get_data.dart
+++ fitness/lib/pages/sample_get_data.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
+import 'package:fitness/pages/sample_crud.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:http/http.dart' as http;
 
 class SampleGetData extends StatefulWidget {
@@ -67,6 +69,29 @@ class _SampleGetDataState extends State<SampleGetData> {
         backgroundColor: Colors.white,
         elevation: 0.0,
         centerTitle: true,
+        actions: [
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SampleCrud()),
+              );
+            },
+            child: Container(
+              margin: const EdgeInsets.all(10),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: const Color(0xffF7F8F8),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: SvgPicture.asset(
+                'assets/icons/dots.svg',
+                height: 5,
+                width: 5,
+              ),
+            ),
+          ),
+        ],
       ),
       backgroundColor: Colors.white,
       body: RefreshIndicator(
